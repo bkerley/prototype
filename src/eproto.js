573ae407a31fd070310458da98109cd75b0b5550
Bryce Kerley <bryce@worldmedia.net>
2008-12-05 14:43:03 -0500

    browserless template

---
+++ src/eproto.js
@@ -0,0 +1,15 @@
+<%= include 'HEADER' %>
+
+var Prototype = {
+  Version: '<%= PROTOTYPE_VERSION %>',
+  
+  JSONFilter: /^\/\*-secure-([\s\S]*)\*\/\s*$/,  
+  
+  emptyFunction: function() { },
+  K: function(x) { return x }
+};
+
+<%= include 'base.js', 'string.js' %>
+
+<%= include 'enumerable.js', 'array.js', 'number.js', 'hash.js', 'range.js' %>
+
