c00f8dc8a54cb959eca0bb9a64d2096dcbe77e95
Bryce Kerley <bryce@worldmedia.net>
2008-12-05 14:55:31 -0500

    need browser features

--- src/eproto.js
+++ src/eproto.js
@@ -3,13 +3,34 @@
 var Prototype = {
   Version: '<%= PROTOTYPE_VERSION %>',
   
+  Browser: {
+    IE:     false,
+    Opera:  false,
+    WebKit: false,
+    Gecko:  true,
+    MobileSafari: false
+  },
+
+  BrowserFeatures: {
+    XPath: false,
+    SelectorsAPI: false,
+    ElementExtensions: false,
+    SpecificElementExtensions: false
+  },
+
+  ScriptFragment: '<script[^>]*>([\\S\\s]*?)<\/script>',
   JSONFilter: /^\/\*-secure-([\s\S]*)\*\/\s*$/,  
   
   emptyFunction: function() { },
   K: function(x) { return x }
 };
 
+if (Prototype.Browser.MobileSafari)
+  Prototype.BrowserFeatures.SpecificElementExtensions = false;
+  
+
 <%= include 'base.js', 'string.js' %>
 
 <%= include 'enumerable.js', 'array.js', 'number.js', 'hash.js', 'range.js' %>
 
+Element.addMethods();
